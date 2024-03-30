96f3a48c4a22b2fd5a59f3d561fd83c4f77f3e93
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 07:54:16 +0900

    update: blog list sample

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href='#'>
+      <article className='shadow my-4 flex flex-col'>
+        <Link href='#' className='hover:opacity-75'>
           <Image
             src='https://source.unsplash.com/collection/1346951/1000x500?sig=1'
             alt=''
@@ -12,17 +12,61 @@ const ArticleList = () => {
             width={1280}
           ></Image>
         </Link>
-        <div>
-          <a href='#'>Technology</a>
-          <a href='#'>Next.js</a>
-          <p>By Kagami, Published on 2024/04/01</p>
-          <Link href='#'>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <a href='#' className='text-blue-700 pb-4 font-bold'>
+            Technology
+          </a>
+          <a
+            href='#'
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.js
+          </a>
+          <p className='text-sm pb-3 text-slate-900'>
+            By Kagami, Published on 2024/04/01
+          </p>
+          <Link href='#' className='text-slate-900 pb-6'>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
             reprehenderit beatae id debitis magni, inventore temporibus
             explicabo omnis fuga veritatis officia? Ex debitis, iste deleniti
             consectetur sequi magnam ducimus veniam?
           </Link>
-          <Link href='#'>More...</Link>
+          <Link href='#' className='text-pink-800 hover:text-black'>
+            More...
+          </Link>
+        </div>
+      </article>
+      <article className='shadow my-4 flex flex-col'>
+        <Link href='#' className='hover:opacity-75'>
+          <Image
+            src='https://source.unsplash.com/collection/1346951/1000x500?sig=2'
+            alt=''
+            height={300}
+            width={1280}
+          ></Image>
+        </Link>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <a href='#' className='text-blue-700 pb-4 font-bold'>
+            Technology
+          </a>
+          <a
+            href='#'
+            className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+          >
+            Next.js
+          </a>
+          <p className='text-sm pb-3 text-slate-900'>
+            By Kagami, Published on 2024/04/01
+          </p>
+          <Link href='#' className='text-slate-900 pb-6'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            reprehenderit beatae id debitis magni, inventore temporibus
+            explicabo omnis fuga veritatis officia? Ex debitis, iste deleniti
+            consectetur sequi magnam ducimus veniam?
+          </Link>
+          <Link href='#' className='text-pink-800 hover:text-black'>
+            More...
+          </Link>
         </div>
       </article>
     </div>
