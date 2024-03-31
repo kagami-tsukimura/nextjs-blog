79c602efd5a9adab75269ef31acb8c6fda000a4d
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 19:28:50 +0900

    feat: get detail Article

--- src/blogApi.ts
+++ src/blogApi.ts
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { Article } from "./types";
 
 export const getAllArticles = async (): Promise<Article[]> => {
@@ -13,4 +14,23 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+export const getDetailArticle = async (id: string): Promise<Article> => {
+  // no-store: ISR
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (res.status == 404) {
+    notFound();
+  } else if (!res.ok) {
+    throw new Error("Error");
+  }
+
+  // loading test
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  const article = await res.json();
+  return article;
 };
