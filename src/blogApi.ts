a29d9bd0e70a46a318fbd3d915c2115a955a97c0
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 12:00:57 +0900

    feat: test error

--- src/blogApi.ts
+++ src/blogApi.ts
@@ -4,6 +4,8 @@ export const getAllArticles = async (): Promise<Article[]> => {
   // no-store: SSR
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" });
 
+  throw new Error("Error");
+
   const articles = await res.json();
   return articles;
 };
