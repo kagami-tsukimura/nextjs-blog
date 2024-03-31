3b462282fb8c7f8b1e0858fc83e1463e3068ff11
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 19:32:18 +0900

    feat: id

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -7,7 +7,7 @@ const Article = async ({ params }: { params: { id: string } }) => {
   return (
     <div className='max-w-3xl mx-auto p-5'>
       <Image
-        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${params.id}`}
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detail.id}`}
         alt=''
         height={300}
         width={1280}
