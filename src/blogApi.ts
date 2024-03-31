98551f58bb0c311685241cd2d23323f44b0b949e
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 12:09:18 +0900

    fix: error refer to response

--- src/blogApi.ts
+++ src/blogApi.ts
@@ -4,7 +4,9 @@ export const getAllArticles = async (): Promise<Article[]> => {
   // no-store: SSR
   const res = await fetch(`http://localhost:3001/posts`, { cache: "no-store" });
 
-  throw new Error("Error");
+  if (!res.ok) {
+    throw new Error("Error");
+  }
 
   const articles = await res.json();
   return articles;
