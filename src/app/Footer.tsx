99e525ce375a8a9fc8ed779579459072a81c3a1e
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 13:10:28 +0900

    style: border color

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='w-full py-2 px-4 border-t'>
+    <footer className='w-full py-2 px-4 border-t border-slate-500'>
       <small>@ 2024 kagami</small>
     </footer>
   );
