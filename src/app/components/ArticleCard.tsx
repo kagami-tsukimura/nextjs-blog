8a43d00e3e9bdb429460218c965bb062b6315501
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 20:41:47 +0900

    refactor: Article Card Component

---
+++ src/app/components/ArticleCard.tsx
@@ -0,0 +1,53 @@
+import { Article } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+
+type ArticleCardProps = {
+  article: Article;
+};
+
+const ArticleCard = ({ article }: ArticleCardProps) => {
+  return (
+    <article className='shadow my-4 flex flex-col' key={article.id}>
+      <Link href={`articles/${article.id}`} className='hover:opacity-75'>
+        <Image
+          src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
+          alt=''
+          height={300}
+          width={1280}
+        ></Image>
+      </Link>
+      <div className='bg-white flex flex-col justify-start p-6'>
+        <Link
+          href={`articles/${article.id}`}
+          className='text-blue-700 pb-4 font-bold'
+        >
+          {article.id}
+        </Link>
+        <Link
+          href={`articles/${article.id}`}
+          className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
+        >
+          {article.title}
+        </Link>
+
+        <p className='text-sm pb-3 text-slate-900'>
+          By Kagami, Published on {article.createdAt}
+        </p>
+        <Link href={`articles/${article.id}`} className='pb-6 text-slate-900'>
+          {article.content.length > 70
+            ? `${article.content.substring(0, 70)}...`
+            : article.content}
+        </Link>
+        <Link
+          href={`articles/${article.id}`}
+          className='text-pink-800 hover:text-black'
+        >
+          More...
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default ArticleCard;
--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,6 +1,5 @@
 import { Article } from "@/types";
-import Image from "next/image";
-import Link from "next/link";
+import ArticleCard from "./ArticleCard";
 
 type ArticleListProps = {
   articles: Article[];
@@ -10,48 +9,7 @@ const ArticleList = ({ articles }: ArticleListProps) => {
   return (
     <div>
       {articles.map((article) => (
-        <article className='shadow my-4 flex flex-col' key={article.id}>
-          <Link href={`articles/${article.id}`} className='hover:opacity-75'>
-            <Image
-              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
-              alt=''
-              height={300}
-              width={1280}
-            ></Image>
-          </Link>
-          <div className='bg-white flex flex-col justify-start p-6'>
-            <Link
-              href={`articles/${article.id}`}
-              className='text-blue-700 pb-4 font-bold'
-            >
-              {article.id}
-            </Link>
-            <Link
-              href={`articles/${article.id}`}
-              className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'
-            >
-              {article.title}
-            </Link>
-
-            <p className='text-sm pb-3 text-slate-900'>
-              By Kagami, Published on {article.createdAt}
-            </p>
-            <Link
-              href={`articles/${article.id}`}
-              className='pb-6 text-slate-900'
-            >
-              {article.content.length > 70
-                ? `${article.content.substring(0, 70)}...`
-                : article.content}
-            </Link>
-            <Link
-              href={`articles/${article.id}`}
-              className='text-pink-800 hover:text-black'
-            >
-              More...
-            </Link>
-          </div>
-        </article>
+        <ArticleCard article={article} key={article.id} />
       ))}
     </div>
   );
