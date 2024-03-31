443ea4850b09885804d89aba43e1bdaef1842bf9
Kagami <shimura.kyohei7617@gmail.com>
2024-03-31 20:51:50 +0900

    update: articles not found message

--- src/app/articles/not-found.tsx
+++ src/app/articles/not-found.tsx
@@ -3,7 +3,7 @@ const NotFound = () => {
     <div className='flex items-center justify-center mt-20'>
       <div className='p-8 rounded-lg shadow-md text-center bg-white'>
         <h1 className='text-2xl font-bold mb-4 text-gray-800'>404</h1>
-        <p className='text-gray-600'>ページが見つかりませんでした。</p>
+        <p className='text-gray-600'>ブログページが見つかりませんでした。</p>
       </div>
     </div>
   );
