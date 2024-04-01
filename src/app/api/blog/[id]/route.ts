177e1d1ab3f486fce38afbd751f1af2c1e8b7253
Kagami <shimura.kyohei7617@gmail.com>
2024-04-01 21:31:11 +0900

    feat: get app

---
+++ src/app/api/blog/[id]/route.ts
@@ -0,0 +1,28 @@
+import { supabase } from "@/utils/supabaseClient";
+import { NextApiResponse } from "next";
+import { notFound } from "next/navigation";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request, res: NextApiResponse) {
+  const id = req.url.split("/blog/")[1];
+
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    return NextResponse.json(error);
+  }
+
+  if (!data) {
+    notFound();
+  }
+
+  if (error) {
+    return NextResponse.json(error);
+  }
+
+  return NextResponse.json(data, { status: 200 });
+}
