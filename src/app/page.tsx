35b5c8c9b45e43462fb34dd74954dd481aef9895
Kagami <shimura.kyohei7617@gmail.com>
2024-04-01 20:36:30 +0900

    delete: comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-// import { getAllArticles } from "@/blogApi";
 import ArticleList from "./components/ArticleList";
 
 export default async function Home() {
-  // const articles = await getAllArticles();
-
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
   const res = await fetch(`${API_URL}/api`, { cache: "no-store" });
 
   const articles = await res.json();
-  console.log(articles);
 
   return (
     <div className='md:flex'>
